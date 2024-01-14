1a481dc0d0d085889c64ce016830ef3869ff9360
Derik Nascimento <79671639+Derik540@users.noreply.github.com>
2024-01-14 10:04:28 -0300

    feat/criando todo list em um arquivo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+
+// Importando CSS do Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Importando JS do Bootstrap
+import 'bootstrap'
 
 createApp(App).mount('#app')
